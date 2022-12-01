26b160b90836f7ba1d7fb1bd45e684cf4cc06c26
joaobuga35 <joaobuga35@gmail.com>
2022-12-01 14:07:49 -0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+// import { RoutesMain as Routes } from "./routes";
+import Global from "./styles/Global";
+function App() {
+  return (
+    <>
+      <Global />
+      <p>Oi</p>
+    </>
+  );
+}
+
+export default App;
